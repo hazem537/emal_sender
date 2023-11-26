8402b55dec4fe9653dabcbe20ba28e4e99a29519
hazem hamdy <hh4morsy@gmail.com>
2023-11-26 14:19:06 +0200

    vercel_deploy

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z(hmc0xs+5u5iw4z3cvlo_vvwdf)&@st#_b^-&)-xpi!ezj_t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",".vercel.app",'.now.sh']
 
 
 # Application definition
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles_build',"static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
