d56608edcd35f748d26b2bed03ff97f17b3fc9d1
hazem hamdy <hh4morsy@gmail.com>
2023-11-26 13:25:49 +0200

    deploy_fly

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z(hmc0xs+5u5iw4z3cvlo_vvwdf)&@st#_b^-&)-xpi!ezj_t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
